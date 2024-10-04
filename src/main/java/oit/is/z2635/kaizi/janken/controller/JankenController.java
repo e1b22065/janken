0c29d0caf723c89aebc388aade72a60ebc94920d
e1b22065 <e1b22065@oit.ac.jp>
2024-10-04 23:00:10 +0900

    じゃんけんの手を選んでCPUと対戦する処理を実装した

--- src/main/java/oit/is/z2635/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2635/kaizi/janken/controller/JankenController.java
@@ -2,6 +2,7 @@ package oit.is.z2635.kaizi.janken.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,4 +21,31 @@ public class JankenController {
     model.addAttribute("username", username);
     return "Janken.html";
   }
+
+  /**
+   * クエリパラメータの引数2つを受け付ける URLでの?のあとのパラメータ名とjavaメソッドの引数名は同じであることが望ましい(別にする方法は一応ある)
+   * 引数をStringとして受け取ってparseIntする以外にもInteger(intのラッパークラス)クラスの変数として受け取ってそのまま加算する方法もある
+   *
+   * @param userhand
+   * @param model
+   * @return
+   */
+  @GetMapping("/jankengame")
+  public String sample23(@RequestParam String hand, ModelMap model) {
+    String Gu = "Gu";
+    String Tyoki = "Tyoki";
+    model.addAttribute("hand", hand);
+    if (hand.equals(Gu)) {
+      model.addAttribute("Result", "Draw");
+    } else if (hand.equals(Tyoki)) {
+      model.addAttribute("Result", "You Lose");
+    } else {
+      model.addAttribute("Result", "You Win!");
+    }
+    // ModelMap型変数のmodelにtasuResult2という名前の変数で，tasuResultの値を登録する．
+    // ここで値を登録するとthymeleafが受け取り，htmlで処理することができるようになる
+    return "janken.html";
+
+  }
+
 }
