9ee753f10f961ef225cfaa1b1f60b43d749703a8
e1b22065 <e1b22065@oit.ac.jp>
2024-10-28 15:11:26 +0900

    対戦相手が手を選び非同期処理で結果の表示

--- src/main/java/oit/is/z2635/kaizi/janken/model/MatchInfoMapper.java
+++ src/main/java/oit/is/z2635/kaizi/janken/model/MatchInfoMapper.java
@@ -3,6 +3,7 @@ package oit.is.z2635.kaizi.janken.model;
 import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
@@ -10,7 +11,7 @@ import org.apache.ibatis.annotations.Options;
 @Mapper
 public interface MatchInfoMapper {
   @Select("SELECT * from matchinfo")
-  ArrayList<Match> selectAllByMatchInfo();
+  ArrayList<MatchInfo> selectAllByMatchInfo();
 
   @Select("SELECT * from matchinfo where true = isActive")
   ArrayList<MatchInfo> selectAllByIsActive();
@@ -18,4 +19,10 @@ public interface MatchInfoMapper {
   @Insert("INSERT INTO MatchInfo (user1, user2, user1Hand, isActive) VALUES (#{user1},#{user2},#{user1Hand},#{isActive});")
   @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
   void insertMatchInfo(MatchInfo MatchInfo);
+
+  @Select("SELECT * FROM MATCHINFO WHERE ID = #{id}")
+  MatchInfo selectById(int id);
+
+  @Update("UPDATE MATCHINFO SET USER1=#{user1}, USER2=#{user2}, USER1HAND=#{user1Hand}, ISACTIVE=#{isActive} WHERE id = #{id}")
+  void updateById(MatchInfo matchInfo);
 }
